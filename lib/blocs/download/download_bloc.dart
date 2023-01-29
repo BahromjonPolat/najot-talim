36bfd30bb1167ef66787eadfaf0fbcd6d3165c80
BahromjonPolat <itjunior.uz@gmail.com>
2023-01-29 20:03:56 +0500

    before merge

--- lib/blocs/download/download_bloc.dart
+++ lib/blocs/download/download_bloc.dart
@@ -14,6 +14,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
