0e200474c181c9ae6a47ceeaa9711f8b7c515789
CoderAltair <kamronqodirov441@gmail.com>
2023-01-29 12:27:12 +0500

    azizbek

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+/*
+
+  Created by: Azizbek
+  Created on: Jan 29 2023 12:00:17
+  Github: https://github.com/CoderAltair
+  Telegram: https://t.me/@azizbek_kv
+
+  Documentation: 
+
+*/
+
 import 'package:flutter/material.dart';
 
 import 'app.dart';
---
+++ lib/view/screens/card/constants/constants.dart
@@ -0,0 +1,15 @@
+/*
+
+  Created by: Azizbek
+  Created on: Jan 29 2023 12:03:15
+  Github: https://github.com/CoderAltair
+  Telegram: https://t.me/@azizbek_kv
+
+  Documentation: 
+
+*/
+
+  enum SingingCharacter { one, two, three, four }
+class ConstantsClass{
+
+}
